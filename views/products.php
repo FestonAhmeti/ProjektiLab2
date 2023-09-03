2c0df3361fed18a82915bf0c8f070159ccb67b39
Feston Ahmeti <fa47928@ubt-uni.net>
2023-09-03 20:00:10 +0200

    Added Mappers

--- views/products.php
+++ views/products.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/rooms.css">
+    <link rel="stylesheet" href="../css/products.css">
 </head>
 <body>
     <?php 
@@ -13,7 +13,50 @@
     ?>
     <div id="main">
         
-        <img src="../images/coming-soon5.png" alt="">
+        <ul id="nav-rooms">
+            <li><a href="">Living Room</a></li>
+            <li><a href="">Kitchen</a></li>
+            <li><a href="">Bedroom</a></li>
+            <li><a href="">Bathroom</a></li>
+            <li><a href="">Dining Room</a></li>
+            <li><a href="">Lightning</a></li>
+
+        </ul>
+
+        <div id="product-filter">
+            <form method="GET" action="<?= $_SERVER['PHP_SELF']?>">
+                <select name="filter">
+                    <option value="all">Të gjitha</option>
+                    <option value="price-low-to-high">Cmimi: poshtë-lart</option>
+                    <option value="price-high-to-low">Cmimi: lart-poshtë</option>
+                    <option value="name-A-Z">Emri: A-Z</option>
+                    <option value="name-Z-A">Emri: Z-A</option>
+                    <option value="newest">Më të fundit</option>
+                    <option value="oldest">Më të vjetrat</option>
+                </select>
+                <input type="submit" value="Filtro">
+            </form>
+        </div>
+
+        <div class="products-container">
+        <div class="products-panel wrapper">
+                <?php //foreach($products as $product){ 
+                    //$pid = $product['id'];
+                ?>
+                <input class="hidden" type="text" name="product_id" value=<?php $pid; ?>> 
+                    <div class="square">
+                        <div>
+                            <a href="<?php //echo "view-product.php?pid=$pid" ?>"><img src=<?php echo $product['image']; ?> alt=""></a>
+                        </div>
+                        <div>
+                            <h3><?php //echo $product['emri']; ?></h3>
+                            <h2><?php //echo $product['cmimi']; ?>&euro;</h2>
+                            <a class="button" href="<?php //echo "view-product.php?pid=$pid" ?>">Shiko Produktin</a>
+                        </div>
+                    </div>
+                <?php //} ?>
+            </div>
+        </div>
 
     </div>
     <?php 
